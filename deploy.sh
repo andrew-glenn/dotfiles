110b6de13a404fc8e6b4c2f4f0f653281d7746ef
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2021-01-05 09:49:41 -0600

    adding bare-bones deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+current_dir=$(pwd)
+ln -s ~/.tmux.conf ${current_dir}/tmux/tmux.conf
+ln -s ~/.tmux.functions.sh ${current_dir}/tmux/tmux.functions.sh
+ln -s ~/.tmux-themepack ${current_dir}/tmux/tmux-themepack
+ln -s ~/.vimrc ${current_dir}/vim/.vimrc
+
