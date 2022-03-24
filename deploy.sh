d769d22f94bf8b7b9323d71edd63ed2b29ac5b22
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2022-03-23 20:27:56 -0500

    add powerlevel10k submodule

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ ln -s ${current_dir}/tmux/tmux.conf ~/.tmux.conf
 ln -s ${current_dir}/tmux/tmux.functions.sh ~/.tmux.functions.sh
 ln -s ${current_dir}/tmux/tmux-themepack ~/.tmux-themepack
 ln -s ${current_dir}/vim/.vimrc ~/.vimrc
-
+ln -s ${current_dir}/zsh/powerlevel10k ~/powerlevel10k
