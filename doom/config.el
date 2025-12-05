e25cffa2b833d36a9cda9cc5b322e7565a223175
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2025-12-05 01:23:08 -0600

    doom: tweaks

--- doom/config.el
+++ doom/config.el
@@ -83,16 +83,16 @@
                            (unusedwrite . t)
                            (useany . t)
                            (unusedvariable . t)))
-)
-;; No splash screen. 
+  )
+;; No splash screen.
 (setq inhibit-startup-message t)
 
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode 1)
 
 ;; Load a theme.
-(load-theme 'modus-vivendi' 1)
-
+(load-theme 'doom-material-dark' t)
+(load-theme 'doom-material' t)
 ;; Recent Open Files
 (recentf-mode 1)
 (winner-mode 1)
@@ -122,140 +122,141 @@
         (3 . (rainbow bold 1.2))
         (t . (semilight 1.1))))
 
-; ;; Enable Evil
-; (require 'evil)
-; (evil-mode 1)
-;
-; ;; Packages. 
-; ;; Set up package.el to work with MELPA
-; (require 'package)
-; (add-to-list 'package-archives
-;              '("melpa" . "https://melpa.org/packages/"))
-; (package-initialize)
-;
-; ;; Download Evil
-; (unless (package-installed-p 'evil)
-;   (package-install 'evil))
-;
-; ;; Download Go-mode
-; (unless (package-installed-p 'go-mode)
-;   (package-install 'go-mode))
-;
-; (require 'go-mode)
-;
-; ;; Set up Go-specific key bindings
-; (add-hook 'go-mode-hook
-;           (lambda ()
-;             (setq tab-width 4)
-;             (setq indent-tabs-mode 1)))
-;
-; ;; Enable auto-completion
-; (add-hook 'go-mode-hook 'company-mode)
-;
-; ;; Enable Flycheck for real-time syntax checking
-; (add-hook 'go-mode-hook 'flycheck-mode)
-;
-; ;; Enable automatic formatting on save
-; (add-hook 'before-save-hook 'gofmt-before-save)
-;
-;
-;
-; (use-package eglot
-;   :ensure t
-;   :defer t
-;   :hook (python-mode . eglot-ensure))
-;
-;
-; (use-package lsp-mode
-;   :ensure t
-;   :commands (lsp lsp-mode lsp-deferred)
-;   :hook ((rust-mode python-mode go-mode) . lsp-deferred)
-;   :config
-;   (setq lsp-prefer-flymake nil
-;         lsp-enable-indentation nil
-;         lsp-enable-on-type-formatting nil
-;         lsp-rust-server 'rust-analyzer)
-;   ;; for filling args placeholders upon function completion candidate selection
-;   ;; lsp-enable-snippet and company-lsp-enable-snippet should be nil with
-;   ;; yas-minor-mode is enabled: https://emacs.stackexchange.com/q/53104
-;   (lsp-modeline-code-actions-mode)
-;   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-;   (add-to-list 'lsp-file-watch-ignored "\\.vscode\\'"))	
-;
-;
-; (require 'project)
-; 	; OSX command key as control
-; (setq mac-command-modifier 'control)
-; (global-set-key (kbd "<select>") 'move-end-of-line) 
-;
-; ; go-fill-struct immediately followed by gofmt
-; (global-set-key (kbd "C-c .") (lambda () (interactive) (go-fill-struct) (gofmt)))
-;
-; (defun my/fake-menu-find-file-existing ()
-;   (interactive)
-;   (let ((use-dialog-box t)
-;          (use-file-dialog t)
-;          (last-nonmenu-event nil))
-;     (menu-find-file-existing)))
-;
+                                        ; ;; Enable Evil
+                                        ; (require 'evil)
+                                        ; (evil-mode 1)
+                                        ;
+                                        ; ;; Packages.
+                                        ; ;; Set up package.el to work with MELPA
+                                        ; (require 'package)
+                                        ; (add-to-list 'package-archives
+                                        ;              '("melpa" . "https://melpa.org/packages/"))
+                                        ; (package-initialize)
+                                        ;
+                                        ; ;; Download Evil
+                                        ; (unless (package-installed-p 'evil)
+                                        ;   (package-install 'evil))
+                                        ;
+                                        ; ;; Download Go-mode
+                                        ; (unless (package-installed-p 'go-mode)
+                                        ;   (package-install 'go-mode))
+                                        ;
+                                        ; (require 'go-mode)
+                                        ;
+                                        ; ;; Set up Go-specific key bindings
+                                        ; (add-hook 'go-mode-hook
+                                        ;           (lambda ()
+                                        ;             (setq tab-width 4)
+                                        ;             (setq indent-tabs-mode 1)))
+                                        ;
+                                        ; ;; Enable auto-completion
+                                        ; (add-hook 'go-mode-hook 'company-mode)
+                                        ;
+                                        ; ;; Enable Flycheck for real-time syntax checking
+                                        ; (add-hook 'go-mode-hook 'flycheck-mode)
+                                        ;
+                                        ; ;; Enable automatic formatting on save
+                                        ; (add-hook 'before-save-hook 'gofmt-before-save)
+                                        ;
+                                        ;
+                                        ;
+                                        ; (use-package eglot
+                                        ;   :ensure t
+                                        ;   :defer t
+                                        ;   :hook (python-mode . eglot-ensure))
+                                        ;
+                                        ;
+                                        ; (use-package lsp-mode
+                                        ;   :ensure t
+                                        ;   :commands (lsp lsp-mode lsp-deferred)
+                                        ;   :hook ((rust-mode python-mode go-mode) . lsp-deferred)
+                                        ;   :config
+                                        ;   (setq lsp-prefer-flymake nil
+                                        ;         lsp-enable-indentation nil
+                                        ;         lsp-enable-on-type-formatting nil
+                                        ;         lsp-rust-server 'rust-analyzer)
+                                        ;   ;; for filling args placeholders upon function completion candidate selection
+                                        ;   ;; lsp-enable-snippet and company-lsp-enable-snippet should be nil with
+                                        ;   ;; yas-minor-mode is enabled: https://emacs.stackexchange.com/q/53104
+                                        ;   (lsp-modeline-code-actions-mode)
+                                        ;   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+                                        ;   (add-to-list 'lsp-file-watch-ignored "\\.vscode\\'"))
+                                        ;
+                                        ;
+                                        ; (require 'project)
+                                        ; 	; OSX command key as control
+                                        ; (setq mac-command-modifier 'control)
+                                        ; (global-set-key (kbd "<select>") 'move-end-of-line)
+                                        ;
+                                        ; ; go-fill-struct immediately followed by gofmt
+                                        ; (global-set-key (kbd "C-c .") (lambda () (interactive) (go-fill-struct) (gofmt)))
+                                        ;
+                                        ; (defun my/fake-menu-find-file-existing ()
+                                        ;   (interactive)
+                                        ;   (let ((use-dialog-box t)
+                                        ;          (use-file-dialog t)
+                                        ;          (last-nonmenu-event nil))
+                                        ;     (menu-find-file-existing)))
+                                        ;
 ;;; lang/go/config.el -*- lexical-binding: t; -*-
 
 ;;
 ;;; Packages
 
-  ; (set-docsets! 'go-mode "Go")
-  ; (set-repl-handler! 'go-mode #'gorepl-run)
-  ; (set-lookup-handlers! 'go-mode
-  ;   :documentation #'godoc-at-point)
-  ;
-  ; (if (modulep! +lsp)
-  ;     (add-hook 'go-mode-local-vars-hook #'lsp! 'append)
-  ;   (add-hook 'go-mode-hook #'go-eldoc-setup))
-  ;
-  ; (when (modulep! +tree-sitter)
-  ;   (add-hook 'go-mode-local-vars-hook #'tree-sitter! 'append))
-  ;
-  ; (map! :map go-mode-map
-  ;       :localleader
-  ;       "a" #'go-tag-add
-  ;       "d" #'go-tag-remove
-  ;       "e" #'+go/play-buffer-or-region
-  ;       "i" #'go-goto-imports      ; Go to imports
-  ;       (:prefix ("h" . "help")
-  ;         "." #'godoc-at-point)    ; Lookup in godoc
-  ;       (:prefix ("ri" . "imports")
-  ;         "a" #'go-import-add)
-  ;       (:prefix ("b" . "build")
-  ;         :desc "go run ." "r" (cmd! (compile "go run ."))
-  ;         :desc "go build" "b" (cmd! (compile "go build"))
-  ;         :desc "go clean" "c" (cmd! (compile "go clean")))
-  ;       (:prefix ("t" . "test")
-  ;         "t" #'+go/test-rerun
-  ;         "a" #'+go/test-all
-  ;         "s" #'+go/test-single
-  ;         "n" #'+go/test-nested
-  ;         "f" #'+go/test-file
-  ;         "g" #'go-gen-test-dwim
-  ;         "G" #'go-gen-test-all
-  ;         "e" #'go-gen-test-exported
-  ;         (:prefix ("b" . "bench")
-  ;           "s" #'+go/bench-single
-  ;           "a" #'+go/bench-all))))
-  ;
-  ;
-; (use-package! gorepl-mode
-;   :commands gorepl-run-load-current-file)
-;
-;
-; (use-package! company-go
-;   :when (modulep! :completion company)
-;   :unless (modulep! +lsp)
-;   :after go-mode
-;   :config
-;   (set-company-backend! 'go-mode 'company-go)
-;   (setq company-go-show-annotation t))
-;
-;
+                                        ; (set-docsets! 'go-mode "Go")
+                                        ; (set-repl-handler! 'go-mode #'gorepl-run)
+                                        ; (set-lookup-handlers! 'go-mode
+                                        ;   :documentation #'godoc-at-point)
+                                        ;
+                                        ; (if (modulep! +lsp)
+                                        ;     (add-hook 'go-mode-local-vars-hook #'lsp! 'append)
+                                        ;   (add-hook 'go-mode-hook #'go-eldoc-setup))
+                                        ;
+                                        ; (when (modulep! +tree-sitter)
+                                        ;   (add-hook 'go-mode-local-vars-hook #'tree-sitter! 'append))
+                                        ;
+                                        ; (map! :map go-mode-map
+                                        ;       :localleader
+                                        ;       "a" #'go-tag-add
+                                        ;       "d" #'go-tag-remove
+                                        ;       "e" #'+go/play-buffer-or-region
+                                        ;       "i" #'go-goto-imports      ; Go to imports
+                                        ;       (:prefix ("h" . "help")
+                                        ;         "." #'godoc-at-point)    ; Lookup in godoc
+                                        ;       (:prefix ("ri" . "imports")
+                                        ;         "a" #'go-import-add)
+                                        ;       (:prefix ("b" . "build")
+                                        ;         :desc "go run ." "r" (cmd! (compile "go run ."))
+                                        ;         :desc "go build" "b" (cmd! (compile "go build"))
+                                        ;         :desc "go clean" "c" (cmd! (compile "go clean")))
+                                        ;       (:prefix ("t" . "test")
+                                        ;         "t" #'+go/test-rerun
+                                        ;         "a" #'+go/test-all
+                                        ;         "s" #'+go/test-single
+                                        ;         "n" #'+go/test-nested
+                                        ;         "f" #'+go/test-file
+                                        ;         "g" #'go-gen-test-dwim
+                                        ;         "G" #'go-gen-test-all
+                                        ;         "e" #'go-gen-test-exported
+                                        ;         (:prefix ("b" . "bench")
+                                        ;           "s" #'+go/bench-single
+                                        ;           "a" #'+go/bench-all))))
+                                        ;
+                                        ;
+                                        ; (use-package! gorepl-mode
+                                        ;   :commands gorepl-run-load-current-file)
+                                        ;
+                                        ;
+                                        ; (use-package! company-go
+                                        ;   :when (modulep! :completion company)
+                                        ;   :unless (modulep! +lsp)
+                                        ;   :after go-mode
+                                        ;   :config
+                                        ;   (set-company-backend! 'go-mode 'company-go)
+                                        ;   (setq company-go-show-annotation t))
+                                        ;
+                                        ;
 ;; Recommended keymap prefix on macOS
-;(projectile-mode +1)
+                                        ;(projectile-mode +1)
 ;;(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(map! "C-w C-w" #'delete-window)
