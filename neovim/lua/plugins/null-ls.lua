1f9b3bece07c3f5e7e18213022cda0e4d7f6930a
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2025-12-12 12:53:29 -0600

    Updating neovim

--- neovim/lua/plugins/null-ls.lua
+++
@@ -1,9 +0,0 @@
-require("null-ls").setup({
-    sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
-        require("null-ls").builtins.diagnostics.mypy,
-        require("null-ls").builtins.diagnostics.flake8
-    },
-})
---
+++ neovim/lua/plugins/themes.lua
@@ -0,0 +1,12 @@
+return {
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "scottmckendry/cyberdream.nvim", lazy = false, priority = 1000 },
+  { "shaunsingh/nord.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+}
+
