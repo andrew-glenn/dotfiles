48fdbeff4d7e275e320fc24f2748e80c1951d664
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2025-12-12 12:53:52 -0600

    update: tmuix

--- tmux/tmux.functions.sh
+++ tmux/tmux.functions.sh
@@ -1,5 +1,3 @@
- set -e
-
  _host_specific_theme(){
   hostname=`hostname`
   if [[ `hostname` =~ dev-dsk ]]; then
@@ -7,15 +5,20 @@
   elif [[ `hostname` =~ radioshack ]]; then 
     tmux source-file "${HOME}/.tmux-themepack/powerline/block/magenta.tmuxtheme"
   else
-    tmux source-file "${HOME}/.tmux-themepack/powerline/block/cyan.tmuxtheme"
+    tmux source-file "${DOTFILES_GIT_REPO}/tmux/.tmux-themepack/powerline/block/red.tmuxtheme"
   fi
 }
  _old_new_status(){
-     set -x
-     while getopts 'x:X:w:g:' opt "$@"; do
+     while getopts 'x:X:w:g:d:n:' opt "$@"; do
          case $opt in
              "g")
-                 _old=$(tmux show -gv $OPTARG)
+              _current=$(tmux show -gv $OPTARG)
+             ;;
+             "d")
+               _default="${OPTARG}"
+             ;;
+             "n")
+              _new="${OPTARG}"
              ;;
              "x")
                  _tmux_command_options=$OPTARG
@@ -37,18 +40,20 @@
    done
    shift $((OPTIND -1))
    new=""
-
-   if [ "$_old" = "on" ]; then
-     new="off"
-   else
-     new="on"
-   fi
+   case "${_current}" in 
+     "${_default}")
+       new="${_new}"
+     ;;
+     "${_new}")
+         new="${_default}"
+     ;;
+  esac
    onsv=$new
    export onsv
    unset _old _tmux_command_options opt
 }
 _toggle_mouse() {
-   _old_new_status -g "mouse"
+   _old_new_status -g "mouse" -d "on" -n "off"
 
    tmux set -qg mouse $onsv \;\
        display "Mouse Mode: [$onsv]"
@@ -58,6 +63,11 @@ _toggle_pane_sync(){
     _old_new_status -x "show-options" -X "wv" -w "synchronize-panes"
     tmux set-window-option -q synchronize-panes $onsv \;\
         display "Pane Sync: [$onsv]"
+}
+
+_toggle_prefix(){
+  _old_new_status -g "prefix" -d "C-b" -n "None"
+  tmux set -qg prefix "${onsv}"
 }
 
  _urlview() {
@@ -77,3 +87,9 @@ _toggle_pane_sync(){
     tmux new-session -A -e "TMUX_SCRATCH_SESSION=true" -s scratch
   fi
  }
+
+_conditional_new_window(){
+  if [[ -d "/Users/" ]]; then 
+    tmux new-window 
+  fi
+}
